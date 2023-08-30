ed94ddd454da5640da60824ebf920ef49218d43b
Ansh Bansal <89405224+anshhhhhhh@users.noreply.github.com>
2023-08-30 14:50:31 +0530

    added a working DB functionality

--- twinning_model_server/admin.py
+++ twinning_model_server/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Data, Field, Sensor
+
+admin.site.register(Data)
+
+admin.site.register(Field)
+
+admin.site.register(Sensor)

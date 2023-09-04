0fd915f0ee1f5d57c37f2c069398293e4bf64992
Aryan Bhosale <f20210274@goa.bits-pilani.ac.in>
2023-09-04 14:59:49 +0530

    input_from_stm

--- getData.py
+++ getData.py
@@ -7,14 +7,28 @@ def read_sensor_data():
     # 'gyroscope_x', 'gyroscope_y', 'gyroscope_z', containing the sensor data.
 
     # For example:
-    sensor_data = {
-        'acceleration_x': 1.0,
-        'acceleration_y': 2.0,
-        'acceleration_z': 3.0,
-        'gyroscope_x': 0.1,
-        'gyroscope_y': 0.2,
-        'gyroscope_z': 0.3,
-    }
+
+    # Sample input string received from the STM32 board
+    input_string = "1.0, 2.0, 3.0, 0.1, 0.2, 0.3"
+
+    # Split the string into individual values
+    values = input_string.split(", ")
+
+    # Define keys for the dictionary
+    keys = ['acceleration_x', 'acceleration_y', 'acceleration_z', 'gyroscope_x', 'gyroscope_y', 'gyroscope_z']
+
+    # Create a dictionary by pairing keys with values
+    sensor_data = {key: float(value) for key, value in zip(keys, values)}
+
+    print(sensor_data)
+    # sensor_data = {
+    #     'acceleration_x': 1.0,
+    #     'acceleration_y': 2.0,
+    #     'acceleration_z': 3.0,
+    #     'gyroscope_x': 0.1,
+    #     'gyroscope_y': 0.2,
+    #     'gyroscope_z': 0.3,
+    # }
     return sensor_data
 
 # Configure the UDP server address and port

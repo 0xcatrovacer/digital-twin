27033720a2471013d13ea60a292869a27f3c350e
Ansh Bansal <89405224+anshhhhhhh@users.noreply.github.com>
2023-09-05 14:36:45 +0530

    API

--- imu_sensor_project/imu_sensor_app/views.py
+++ imu_sensor_project/imu_sensor_app/views.py
@@ -1,11 +1,19 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from django.shortcuts import render
 from .models import SensorData
+from .getData import read_sensor_data
 
-def sensor_data(request):
-    sensor_data = SensorData.objects.order_by('-timestamp').first()
+class SensorDataAPI(APIView):
+    def post(self, request):
+        sensor_data = read_sensor_data()  # Call your sensor data reading function
 
-    context = {
-        'sensor_data': sensor_data,
-    }
+        if not sensor_data:
+            return Response({"message": "No sensor data received"}, status=status.HTTP_400_BAD_REQUEST)
 
-    return render(request, 'imu_sensor_app/sensor_data.html', context)
+        try:
+            SensorData.objects.create(**sensor_data)  # Store the sensor data in the database
+            return Response({"message": "Sensor data received and saved successfully"}, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response({"message": f"Error: {str(e)}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
--- imu_sensor_project/imu_sensor_project/urls.py
+++ imu_sensor_project/imu_sensor_project/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from imu_sensor_app import views
+from imu_sensor_app.views import SensorDataAPI
 
 urlpatterns = [
+    path('api/sensor-data/', SensorDataAPI.as_view(), name='sensor-data-api'),
     path('admin/', admin.site.urls),
-    path('', views.sensor_data, name='root'),  # Redirect root URL to sensor_data
-    path('sensor_data/', views.sensor_data, name='sensor_data'),
+    path('', views.SensorData, name='root'),  # Redirect root URL to sensor_data
+    path('sensor_data/', views.SensorData, name='sensor_data'),
 ]

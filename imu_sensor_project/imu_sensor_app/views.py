7e2473ca67c438919075d3ba32a429da8c18ce52
Ansh Bansal <89405224+anshhhhhhh@users.noreply.github.com>
2023-09-06 12:18:06 +0530

    debugged API

--- imu_sensor_project/imu_sensor_app/views.py
+++ imu_sensor_project/imu_sensor_app/views.py
@@ -3,11 +3,23 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.shortcuts import render
 from .models import SensorData
-from .getData import read_sensor_data
 
 class SensorDataAPI(APIView):
     def post(self, request):
-        sensor_data = read_sensor_data()  # Call your sensor data reading function
+        try:
+            
+            input_string = request.body.decode("utf-8")
+
+            
+            values = input_string.split(" ")
+
+            
+            keys = ['acceleration_x', 'acceleration_y', 'acceleration_z', 'gyroscope_x', 'gyroscope_y', 'gyroscope_z']
+
+            
+            sensor_data = {key: float(value) for key, value in zip(keys, values)}
+        except (ValueError, UnicodeDecodeError):
+            return Response({"message": "Invalid sensor data format"}, status=status.HTTP_400_BAD_REQUEST)
 
         if not sensor_data:
             return Response({"message": "No sensor data received"}, status=status.HTTP_400_BAD_REQUEST)
@@ -17,6 +29,8 @@ class SensorDataAPI(APIView):
             return Response({"message": "Sensor data received and saved successfully"}, status=status.HTTP_201_CREATED)
         except Exception as e:
             return Response({"message": f"Error: {str(e)}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
 
 def sensor_data(request):
     sensor_data = SensorData.objects.order_by('-timestamp').first()

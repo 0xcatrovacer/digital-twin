8123bccfd4f9144a8121c70539940c0f65641588
Aryan Bhosale <f20210274@goa.bits-pilani.ac.in>
2023-09-07 15:55:42 +0530

    3D DONE LFG

--- imu_sensor_project/imu_sensor_app/test_api.py
+++ imu_sensor_project/imu_sensor_app/test_api.py
@@ -8,9 +8,9 @@ sensor_data = {
     'acceleration_x': 1.0,
     'acceleration_y': 2.0,
     'acceleration_z': 3.0,
-    'gyroscope_x': 0.1,
-    'gyroscope_y': 1.0,
-    'gyroscope_z': 1.0,
+    'gyroscope_x': 1.1,
+    'gyroscope_y': 5.0,
+    'gyroscope_z': 3.0,
 }
 
 values = [str(value).encode('utf-8') for value in sensor_data.values()]

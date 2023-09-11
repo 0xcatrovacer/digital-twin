5bb9a13a1bc296118d5a5c8b798b7d2118a47dd7
Aryan Bhosale <f20210274@goa.bits-pilani.ac.in>
2023-09-11 14:56:17 +0530

    channels_update

--- imu_sensor_project/imu_sensor_app/views.py
+++ imu_sensor_project/imu_sensor_app/views.py
@@ -3,20 +3,19 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.shortcuts import render
 from .models import SensorData
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+
+def send_sensor_data(sensor_data):
+    channel_layer = get_channel_layer()
+    async_to_sync(channel_layer.group_send)("sensor_data_group", {"type": "sensor_data.update", "data": sensor_data})
 
 class SensorDataAPI(APIView):
     def post(self, request):
         try:
-            
-            input_string = request.body.decode("utf-8")
-
-            
+            input_string = request.body.decode("utf-8")        
             values = input_string.split(" ")
-
-            
             keys = ['acceleration_x', 'acceleration_y', 'acceleration_z', 'gyroscope_x', 'gyroscope_y', 'gyroscope_z']
-
-            
             sensor_data = {key: float(value) for key, value in zip(keys, values)}
         except (ValueError, UnicodeDecodeError):
             return Response({"message": "Invalid sensor data format"}, status=status.HTTP_400_BAD_REQUEST)
@@ -26,9 +25,11 @@ class SensorDataAPI(APIView):
 
         try:
             SensorData.objects.create(**sensor_data)  # Store the sensor data in the database
+            send_sensor_data(sensor_data)
             return Response({"message": "Sensor data received and saved successfully"}, status=status.HTTP_201_CREATED)
         except Exception as e:
             return Response({"message": f"Error: {str(e)}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
 
 
 
